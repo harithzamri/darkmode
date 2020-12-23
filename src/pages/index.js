e6b2f9a3400467fb0097cd404df2c1637605b91e
Harith Zamri <harithtoikee96@gmail.com>
2020-12-23 09:25:41 +0800

    manage to post data inside the database using axios

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,39 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Container } from "../components/Container";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { Button, Text, Flex, Image, useColorMode } from "@chakra-ui/react";
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
+const Index = () => {
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
+  return (
+    <Container height="100vh">
+      <DarkModeSwitch />
+      {isDark ? (
+        <Image
+          src="https://greatech-group.com/images/logo-greatech-color.png"
+          filter="brightness(1.5)"
+          alt="greatech bright mode"
+          width="180px"
+        />
+      ) : (
+        <Image
+          src="https://greatech-group.com/images/logo-greatech.png"
+          alt="greatech dark mode"
+          width="180px"
+        />
+      )}
+      <Text fontSize="m" marginTop="5rem">
+        Lets Work Together to keep Greatech Covid-19 Free !
       </Text>
+      <Flex marginTop="1rem">
+        <Button colorScheme="blue" marginRight="2rem">
+          Vendors
+        </Button>
+        <Button colorScheme="blue">Employee</Button>
+      </Flex>
+    </Container>
+  );
+};
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
-
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
-
-export default Index
+export default Index;
